dc45cbaa10e3a98a9ef5bbbc8245fc3b75f7c688
Leo <luoguofeng0401@gmail.com>
2026-06-18 06:51:37 +0800

    Initial Commit

---
+++ Bookkeeping/BookkeepingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookkeepingApp.swift
+//  Bookkeeping
+//
+//  Created by Guofeng Luo on 2026-06-18.
+//
+
+import SwiftUI
+
+@main
+struct BookkeepingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookkeeping/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookkeeping
+//
+//  Created by Guofeng Luo on 2026-06-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
