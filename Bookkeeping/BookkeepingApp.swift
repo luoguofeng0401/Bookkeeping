a10f44fc5af9584cee2c2f76f49f8e4453e7083f
Leo <luoguofeng0401@gmail.com>
2026-06-18 08:13:22 +0800

    creat ContentView and TodoListView

--- Bookkeeping/BookkeepingApp.swift
+++ Bookkeeping/BookkeepingApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BookkeepingApp: App {
@@ -13,5 +14,6 @@ struct BookkeepingApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: TodoItem.self)
     }
 }
--- Bookkeeping/Views/ContentView.swift
+++ Bookkeeping/Views/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var context
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TodoListView(vm: TodoViewModel(context: context))
     }
 }
 
