dc67bdc958f3338ecad08dce4989027c046e7dc5
Leo <luoguofeng0401@gmail.com>
2026-06-23 04:51:39 +0800

    only views not completd

---
+++ Bookkeeping/Models/TodoItem.swift
@@ -0,0 +1,22 @@
+//
+//  TodoItem.swift
+//  Bookkeeping
+//
+//  Created by Guofeng Luo on 2026-06-23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class TodoItem {
+    var title: String
+    var isCompleted: Bool
+    var createdAt: Date
+    
+    init(title: String) {
+        self.title = title
+        self.isCompleted = false
+        self.createdAt = Date()
+    }
+}
